6b878523bb6608862ef1c62c668a33f23f66f13d
SaloniPatel <saloni.patel01@gmail.com>
2020-08-30 13:13:58 +0530

    Task completed

---
+++ Memory Game/script.js
@@ -0,0 +1,85 @@
+const cards = document.querySelectorAll('.memory-card');
+const score=document.getElementById("point");
+const finalScore=document.getElementById("finalPoints");
+const won=document.getElementById("won");
+
+var points=0;
+var finalPoint=0;
+var win=0;
+
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  isMatch();
+}
+
+function isMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  points+=4;
+  win+=2;
+  finalScore.innerHTML=finalPoint;
+  score.innerHTML=points;
+
+  if(win==12){
+    won.style.visibility="visible";
+  }
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 900);
+  points-=1;
+  finalPoint=points;
+  score.innerHTML=points;
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function doReload(){
+  location.reload();
+}
+//IIFE
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+      
